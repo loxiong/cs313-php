bda857bcd838f737857132e0dccc9982090e06e6
chinglo-x <cxiong48@gmail.com>
2019-09-28 08:49:44 -0500

    fix footer layout

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,3 +1,3 @@
-<p>&copy; CLO, All Rights Reserved. All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.</p>
-<?php echo date('l, F d, Y');?>
+<p>&copy; CLO, All Rights Reserved. | All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed. | <?php echo date('l, F d, Y');?></p>
+
 

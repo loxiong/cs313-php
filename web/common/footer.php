5ebd07753ce24311c30a79e8147a9f18a83ebb26
chinglo-x <cxiong48@gmail.com>
2019-09-28 09:26:26 -0500

    fix footer

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,3 +1,4 @@
-<p>&copy; CLO, All Rights Reserved. | All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed. | <?php echo date('l, F d, Y');?></p>
+<p>&copy; CLO, All Rights Reserved. | All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed. </p>
+<p><?php echo date('l, F d, Y');?></p>
 
 

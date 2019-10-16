6a64e40494882aa26a6605976cc60c32809cbb47
chinglo-x <cxiong48@gmail.com>
2019-10-15 22:40:26 -0500

    test php scripture form

--- web/week5/teamweek5form.php
+++ web/week5/teamweek5form.php
@@ -32,21 +32,34 @@ $result = pg_query($query);
     <title>Scripture</title>
     </head>
 <body>
-<form action="teamweek5.php" method="post">
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+        <select name="scripture-input">
+          <option value="John">John</option>
+          <option value="Mosiah">Mosiah</option>
+          <option value="D&C">Doctrine and Covenants</option>
+          <option value="all">See the List</option>
+        </select>
+        <input type="submit" />
+      </form>
 
-    <label for="book">BOOK</label>
-    <input type="text" name="book" id="book">
-
-    <label for="chapter">CHAPTER</label>
-    <input type="text" name="chapter" id="chapter">
-
-    <label for="verse">VERSE</label>
-    <input type="text" name="verse" id="verse">
-
-    <label for="content">CONTENT</label>
-    <input type="text" name="content" id="content">
-
-    <input type="submit" value="Submit">
-</form>
+      <?php
+        if (isset($_POST["scripture-input"]) && $_POST["scripture-input"] != "all") {
+          $stmt = $db->prepare('SELECT scriptures_id, book, chapter, verse, content FROM scriptures WHERE book=:book');
+          $stmt->bindValue(':book', $_POST["scripture-input"], PDO::PARAM_STR);
+          $stmt->execute();
+        }
+        else {
+          $stmt = $db->query('SELECT scriptures_id, book, chapter, verse, content FROM scriptures');
+        }
+        $count = 0;
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+          echo '<div>';
+          echo '<p><a href="./teamweek5details.php?content=' . $row['scripture_id'] . '">';
+          echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':';
+          echo $row['verse'] . '</strong>';
+          echo '</a></p>';
+          echo '</div>';
+        }
+    ?>
 </body>
 </html>

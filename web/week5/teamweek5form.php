66938b2a03f389396d1fc7291eb4a265b3ea2cc1
chinglo-x <cxiong48@gmail.com>
2019-10-15 22:12:44 -0500

    php form

--- web/week5/teamweek5form.php
+++ web/week5/teamweek5form.php
@@ -1,5 +1,22 @@
-
 <?php
+//PDO CONNECTION
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+  $dbOpts = parse_url($dbUrl);
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 $db = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=");
 $query = "INSERT INTO book VALUES ('$_POST[book]','$_POST[chapter]',
 '$_POST[verse]','$_POST[content]')";
@@ -13,16 +30,10 @@ $result = pg_query($query);
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Lets go Shopping</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="pricing.css" rel="stylesheet">
-  </head>
-<body class="container">
-<form action="insert.php" method="post">
+    <title>Scripture</title>
+    </head>
+<body>
+<form action="teamweek5.php" method="post">
 
     <label for="book">BOOK</label>
     <input type="text" name="book" id="book">

6dd765fb8d1db772f8e28c4332e4525c12934a0e
chinglo-x <cxiong48@gmail.com>
2019-10-17 04:59:28 -0500

    php queries

--- web/week5/details.php
+++ web/week5/details.php
@@ -18,17 +18,15 @@ catch (PDOException $ex)
   echo 'Error!: ' . $ex->getMessage();
   die();
 }
-$id = -1;
-if(isset($_GET["id"]))
-{
-$id = $_GET["id"];
-}
-if (isset($db) && $id > 0)
-{
-$statement = $db->prepare('SELECT * FROM scriptures WHERE id=:id');
-$statement->execute(array(':id' => $id));
-$result = $statement->fetch(PDO::FETCH_ASSOC);
-}
+//$id = -1;
+//if(isset($_GET["id"])){
+//$id = $_GET["id"];
+//}
+//if (isset($db) && $id > 0){
+//$statement = $db->prepare('SELECT * FROM scriptures WHERE id=:id');
+//$statement->execute(array(':id' => $id));
+//$result = $statement->fetch(PDO::FETCH_ASSOC);
+//}
 ?>
 <!DOCTYPE html>
 <html lang="en">

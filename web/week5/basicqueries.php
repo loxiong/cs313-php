daf108b8d122a5e65af7bf19165c31b467a03d36
chinglo-x <cxiong48@gmail.com>
2019-10-17 23:27:03 -0500

    php queries

--- web/week5/basicqueries.php
+++ web/week5/basicqueries.php
@@ -69,7 +69,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
             </ul>
         <?php
             //$db = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=myadmin123");
-            $statement = $db->prepare("INSERT INTO note (userId, content) VALUES ('$_POST[userId]','$_POST[content]')";
+            $statement = $db->prepare("INSERT INTO note (userId, content) VALUES ('$_POST[userId]','$_POST[content]')");
             $statement->execute();
             $query = "INSERT INTO note (userId, content) VALUES ('$_POST[userId]','$_POST[content]')";
             $result = pg_query($query); 

aba5ad676718e5905ea13f5a07f06617d7a056f6
chinglo-x <cxiong48@gmail.com>
2019-10-23 23:41:43 -0500

    scripture id

--- web/week6/scripture_content.php
+++ web/week6/scripture_content.php
@@ -3,18 +3,10 @@
 // Information can come from part of a REQUEST to the other page 
 // GET request to retrieve data (to get things)
 // POST request is used when you want to change thinngs in a certain way
-// the goal is to get in the URL to show "../scripture.php?scripture_id=1"
+// the goal is to get in the URL to show "../scripture.php?scriptures_id=1"
 // So we will use the table column id to get and point to the content we need
 // To achieve this, go back to the scripture.php page and make the echo a link-->
 <?php
-require('dbConnect.php');
-// define the variable that was created on the scripture.php page in the link
-// $scripture_id = $_GET['scriptures_id'];
-// can wrap it in an isset to make in more interesting
-if (!isset($_GET['scriptures_id']))
-{
-    die("Error: scripture id not specified")
-}
 $scriptures_id = htmlspecialchars($_GET['scriptures_id']); //add htmlspecialchars to check the integrity of the data
 ?>
 <!DOCTYPE html>
@@ -27,7 +19,7 @@ $scriptures_id = htmlspecialchars($_GET['scriptures_id']); //add htmlspecialchar
     
     <body>
         <header>
-            <h1>Scripture Content <?php var_dump $scriptures_id ?></h1>
+            <h1>Scripture Content <?php echo $scriptures_id ?></h1>
         </header>    
         
         <main>

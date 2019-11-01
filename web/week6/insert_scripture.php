fc5c0f1ebe06e42a403f9a841d65d99f4b485084
chinglo-x <cxiong48@gmail.com>
2019-11-01 13:09:07 -0500

    php queries

--- web/week6/insert_scripture.php
+++ web/week6/insert_scripture.php
@@ -50,7 +50,7 @@ catch (Exception $ex)
 	die();
 }
 // finally, redirect them to a new page to actually show the topics
-header("Location: final_scripture.php");
+header("Location: scripture.php");
 die(); // we always include a die after redirects. In this case, there would be no
        // harm if the user got the rest of the page, because there is nothing else
        // but in general, there could be things after here that we don't want them

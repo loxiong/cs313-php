2b7b8411ea4953b9766d77a3f79cd847a2e0a75a
chinglo-x <cxiong48@gmail.com>
2019-11-01 02:26:59 -0500

    php queries

--- web/week6/script_content.php
+++ web/week6/script_content.php
@@ -24,11 +24,6 @@ $stmt->bindValue(':id', $scriptures_id, PDO::PARAM_INT);
 $stmt->execute();
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $script_book=$rows[0]['book'];
-foreach ($rows as $row)
-    {
-        $chapter = $row['chapter'];
-        $content = $row['content'];
-    }
 
 /*$query = 'SELECT id, book, chapter, verse, content FROM scriptures';
 $stmt = $db->prepare($query);
@@ -65,26 +60,21 @@ $content_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
         
         <main>
             <h1>Scripture Content <?php echo $scriptures_id; ?></h1>
-            <h1>Scripture Content for <?php echo $script_book $chapter; ?></h1>
+            <h1>Scripture Content for <?php echo $script_book; ?></h1>
             
-            <p>
+            <ul style="list-style-type:none">
             <?php
             foreach ($rows as $row)
             {
+                $chapter = $row['chapter'];
+                $verse = $row['verse'];
                 $content = $row['content'];
+                echo "<h2>$chapter: $verse</h2>";
                 echo "<p>$content</p>";
             }
             ?>
-            </p>
-                $id = htmlspecialchars($_GET["scriptures_id"]);
-            $row = ($db->query("SELECT * FROM scriptures WHERE id='$id'"))[0];
-            $book = $row["book"];
-            $chapter = $row["chapter"];
-            $verse = $row["verse"];
-            $content = $row["content"];
-            echo("$book $chapter:$verse &ldquo;$content&rdquo;");
-                ?>
-            
+            </ul>
+
         </main>
     
     </body>

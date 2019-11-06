15dafc7a4b48f1330afe0dd18d0a094524be4be8
chinglo-x <cxiong48@gmail.com>
2019-11-05 18:45:55 -0600

    php user

--- web/myProject/index.php
+++ web/myProject/index.php
@@ -3,86 +3,60 @@
 * File: index.php (the sign in page)
 * Description: This page has a form for the user to sign in.
 ***********************************************************/
-session_start();
-$badLogin = false;
-// First check to see if we have post variables, if not, just
-// continue on as always.
-if (isset($_POST['txtUser']) && isset($_POST['txtPassword']))
-{
-	// they have submitted a username and password for us to check
-	$username = $_POST['txtUser'];
-	$password = $_POST['txtPassword'];
-	// Connect to the DB
-	require("dbconnect.php");
-	$db = get_db();
-	$query = 'SELECT password FROM fakepeople WHERE username=:username';
-	$statement = $db->prepare($query);
-	$statement->bindValue(':username', $username);
-	$result = $statement->execute();
-	if ($result)
-	{
-		$row = $statement->fetch();
-		$hashedPasswordFromDB = $row['password'];
-		// now check to see if the hashed password matches
-		if (password_verify($password, $hashedPasswordFromDB))
-		{
-			// password was correct, put the user on the session, and redirect to home
-			$_SESSION['username'] = $username;
-			header("Location: home.php");
-			die(); // we always include a die after redirects.
-		}
-		else
-		{
-			$badLogin = true;
-		}
-	}
-	else
-	{
-		$badLogin = true;
-	}
-}
+<?php
+    session_start();
+    require("redirects.php");
+    $user = $_SESSION["user"];
+    $name = $_SESSION["name"];
+    if (isset($user)) {
+        loginSuccess($user, $name);
+    }
+    $valid = $_SESSION["valid-credentials"];
+    if (isset($valid) || !empty($valid)) {
+        $valid = htmlspecialchars(trim($valid));
+        if ($valid === "false") {
+            $valid = false;
+        }
+    } else {
+        $valid = null;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
     <head>
         <title>Database | Login</title>
-        <!--reset.css will remove all the browser's default styles-->
-        <link href="css/reset.css" rel="stylesheet" type="text/css"> 
         <link href="css/styles.css" rel="stylesheet">  
     </head>
         <body>
-            <h1>Concession Planner</h1>                   
-            <span>Login</span>
-            <hr />
-            
-            <div>
-            <?php
-            if ($badLogin)
-            {
-                echo "Incorrect username or password!<br /><br />\n";
-            }
-            ?>
-            <h1>Please sign in below:</h1>
-
-            <form id="mainForm" action="signin.php" method="POST">
-
-                <label for="txtUser">Username</label>
-                <input type="text" id="txtUser" name="txtUser" placeholder="Username">
-                <br /><br />
-
-                <label for="txtPassword">Password</label>
-                <input type="password" id="txtPassword" name="txtPassword" placeholder="Password">
-                <br /><br />
-
-                <input type="submit" value="Sign In" />
-
-            </form>
-
-            <br /><br />
-
-            Or <a href="signup.php">Sign up</a> for a new account.
-
-            </div>
+                <div>
+                    <h1>Concession Planner</h1>
+                    <form method="POST">
+                    <span>Login</span>
+                    <hr />
+                    <table>
+                    <?php if ($valid === false): ?>
+                    <tr>
+                        <td colspan="2">
+                            <span style="color: #f00;">Invalid username or password</span>
+                        </td>
+                    </tr>
+                    <?php endif; ?>
+                    <tr>
+                        <td>Username</td>
+                        <td><input type="text" name="username"  /></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" name="password" /></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <input type="submit" class="button" value="Log In" formaction="login.php" />
+                        </td>
+                    </tr>
+                    </table>
+                    </form>
+                </div>
         </body>
 </html>
 <?php

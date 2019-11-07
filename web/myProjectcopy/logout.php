1d7cab8b6fdfb746a9f1aaaddccce15f6024f960
chinglo-x <cxiong48@gmail.com>
2019-11-07 00:07:21 -0600

    php

--- web/myProjectcopy/logout.php
+++ web/myProjectcopy/logout.php
@@ -1,5 +1,15 @@
 <?php
-    session_start();
-    require("redirects.php");
-    logout();
+function logout() {
+        $_SESSION["username"] = null;
+        $_SESSION["password"] = null;
+        unset($_SESSION["username"]);
+        loginRedirect();
+}
+function loginRedirect() {
+    $_SESSION["valid-credentials"] = null;
+    header("Location: home.php");
+    exit;
+}
+
 ?>
+
--- web/myProjectcopy/signout.php
+++ web/myProjectcopy/signout.php
@@ -1,22 +1,5 @@
 <?php
-
-require("password.php"); // used for password hashing.
-session_start();
-unset($_SESSION['username']);
-header("Location: home.php");
-die();
-
-function logout() {
-        $_SESSION["username"] = null;
-        $_SESSION["password"] = null;
-        unset($_SESSION["username"]);
-        loginRedirect();
-}
-function loginRedirect() {
-    $_SESSION["valid-credentials"] = null;
-    header("Location: home.php");
-    exit;
-}
-
+    session_start();
+    require("logout.php");
+    logout();
 ?>
-    
